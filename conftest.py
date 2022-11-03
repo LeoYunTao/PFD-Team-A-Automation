54e17c4d2c27e21c361ed6f1ed84b1d44a695063
Leo Yun Tao <Leoyuntao@gmail.com>
2022-11-03 17:33:42 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -27,33 +27,39 @@ def os_system(request):
 @pytest.fixture
 def driver(browser, os_system):
 
-    browser_dict = {
-        "firefox": {
-            "browser_file": "firefox.exe",
-            "driver_file": "geckodriver.exe",
-            "path": f"drivers/{os_system}/Firefox"
-        },
-        "chrome": {
-            "browser_file": "chrome.exe",
-            "driver_file": "chromedriver.exe",
-            "path": f"drivers/{os_system}/Chrome"
+    if os_system == 'windows':
+        browser_dict = {
+            "firefox": {
+                "browser_file": "firefox.exe",
+                "driver_file": "geckodriver.exe",
+                "path": f"drivers/{os_system}/Firefox"
+            },
+            "chrome": {
+                "browser_file": "chrome.exe",
+                "driver_file": "chromedriver.exe",
+                "path": f"drivers/{os_system}/Chrome"
+            }
         }
-    }
 
-    browser_file_path = os.path.abspath(browser_dict[browser]['path'])
-    
-    if browser == "firefox":
-        options = firefox.options.Options()
-        options.binary_location = os.path.join(browser_file_path, browser_dict[browser]['browser_file'])
+        browser_file_path = os.path.abspath(browser_dict[browser]['path'])
+        
+        if browser == "firefox":
+            options = firefox.options.Options()
+            options.binary_location = os.path.join(browser_file_path, browser_dict[browser]['browser_file'])
 
-        service = firefox.service.Service(os.path.join(browser_file_path, browser_dict[browser]['driver_file']))
+            service = firefox.service.Service(os.path.join(browser_file_path, browser_dict[browser]['driver_file']))
 
-        return webdriver.Firefox(options=options, service=service)
+            return webdriver.Firefox(options=options, service=service)
+        else:
+            options = chrome.options.Options()
+            options.binary_location = os.path.join(browser_file_path, browser_dict[browser]['browser_file'])
+
+            service = chrome.service.Service(os.path.join(browser_file_path, browser_dict[browser]['driver_file']))
+
+            return webdriver.Chrome(options=options, service=service)
     else:
-        options = chrome.options.Options()
-        options.binary_location = os.path.join(browser_file_path, browser_dict[browser]['browser_file'])
-
-        service = chrome.service.Service(os.path.join(browser_file_path, browser_dict[browser]['driver_file']))
-
-        return webdriver.Chrome(options=options, service=service)
+        if browser == "firefox":
+            return webdriver.Firefox()
+        else:
+            return webdriver.Chrome()
     
