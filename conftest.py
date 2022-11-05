0135981a2c4c18b9c3d44a13b893d0d0f509db2a
Leo Yun Tao <Leoyuntao@gmail.com>
2022-11-06 01:22:47 +0800

    crash fix

--- conftest.py
+++ conftest.py
@@ -12,10 +12,6 @@ def pytest_addoption(parser):
         '--browser', action='store', 
         default='chrome', help='The browser to perform testing on'
     )
-    # parser.addoption(
-    #     '--os', action='store', 
-    #     default='windows', help='The os that the testing will be performed on'
-    # )
     parser.addoption(
         '--production', action='store', 
         default='false', help='See if the code is in production or testing mode'
@@ -24,10 +20,6 @@ def pytest_addoption(parser):
 @pytest.fixture
 def browser(request):
     return request.config.getoption('--browser')
-
-# @pytest.fixture
-# def os_system(request):
-#     return request.config.getoption('--os')
 
 @pytest.fixture
 def production(request):
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
         raise Exception("OS not found")
 
     for browser in os_browser[current_os]:
-        retcode = pytest.main(['test_cases/', f'--browser={browser}', f'-n {N_JOBS}', '--production=true', '-k \'TestRegister\''])
+        retcode = pytest.main(['test_cases/', f'--browser={browser}', '--production=true', f'-n {N_JOBS}'])
 
 if __name__ == '__main__':
     main()
