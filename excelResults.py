95bc76d1c592b740f45f8c38034aa83a2fbee5ba
Leo Yun Tao <Leoyuntao@gmail.com>
2022-11-13 02:28:07 +0800

    changed main.yml and excel results

--- excelResults.py
+++ excelResults.py
@@ -1,11 +1,27 @@
 import pandas as pd
 import time
-timestr = time.strftime("%Y%m%d-%H%M%S")
-df_windows = pd.read_csv('testswindows.csv')
-df_linux = pd.read_csv('testslinux.csv')
-df_mac = pd.read_csv('testsmac.csv')
-with pd.ExcelWriter("Test_Results_"+timestr+".xlsx") as writer:
-    df_windows.to_excel(writer, sheet_name="Windows")
-    df_linux.to_excel(writer, sheet_name="Linux")
-    df_mac.to_excel(writer, sheet_name="Mac")
+import glob
+import platform
 
+current_os = platform.system()
+if current_os == "Windows":
+    current_os = current_os.lower()
+elif current_os == "Linux":
+    current_os = current_os.lower()
+elif current_os == "Darwin":
+    current_os = "macos"
+else:
+    raise Exception("OS not found")
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+df_test_results = []
+
+# find all the csv files in the directory
+for file in glob.glob('csv-reports/*.csv'):
+    df_test_results.append(pd.read_csv(file))
+
+# convert it into excel file
+with pd.ExcelWriter("excel-report/Test_Results_" + timestr + ".xlsx") as writer:
+    for df_test_result in df_test_results:
+        df_test_results.to_excel(writer, sheet_name=current_os)
