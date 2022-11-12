fcb3030121461232f2126a6fc090f60de0193027
Leo Yun Tao <Leoyuntao@gmail.com>
2022-11-13 02:35:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
 
     retcode = pytest.main(['test_cases/', '--production=true', 
         f'-n {N_JOBS}', f'--alluredir=allure-results/{current_os}/',
-        f'csv csv-reports/test_{current_os}.csv --csv-columns id, function, status, duration, message, parameters_as_columns'])
+        f'--csv csv-reports/test_{current_os}.csv', '--csv-columns id, function, status, duration, message, parameters_as_columns'])
     #pytest test_cases/ -s -n 5 --csv csv-reports/testswindows.csv --csv-columns id, function, status, duration, message, parameters_as_columns
 
 if __name__ == '__main__':
