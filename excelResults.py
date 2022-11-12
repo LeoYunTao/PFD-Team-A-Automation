c3d30f8f74e5a8fded489771466f8e5ac7f58228
Leo Yun Tao <Leoyuntao@gmail.com>
2022-11-13 03:00:35 +0800

    Update excelResults.py

--- excelResults.py
+++ excelResults.py
@@ -2,6 +2,7 @@ import pandas as pd
 import time
 import glob
 import platform
+import os
 
 current_os = platform.system()
 if current_os == "Windows":
@@ -20,6 +21,9 @@ df_test_results = []
 # find all the csv files in the directory
 for file in glob.glob('csv-reports/*.csv'):
     df_test_results.append(pd.read_csv(file))
+
+if not os.path.exists('excel-report'):
+   os.makedirs('excel-report')
 
 # convert it into excel file
 with pd.ExcelWriter("excel-report/Test_Results_" + timestr + ".xlsx") as writer:
