2a54d7b103d1eaf58b39dbf927a86511c6910c7c
TerrenceEng <S10223166@connect.np.edu.sg>
2022-11-15 09:40:16 +0800

    Finished Log In test case and started on apply loan test case

--- test_cases/test_applyLoan/test_applyLoan.py
+++ test_cases/test_applyLoan/test_applyLoan.py
@@ -1,27 +1,32 @@
 import pytest
+from logging import error
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-
 class TestApplyLoan:
-    def test_login(self, driver):
+    def test_applyloan(self, driver):
         # log in credentials
-        username = "Terrence"
-        password = "T049111F"
+        email = "Terrence_eng@hotmail.com"
+        amount = 100000
+        term = 1
+        income = 200000
+        age = 18
 
-        # head to github login page
-        driver.get("https://uibank.uipath.com/login")
+        # head to uibank login page
+        driver.get("https://uibank.uipath.com/loans/apply")
         # find username/email field and send the username itself to the input field
-        driver.find_element(By.ID, "username").send_keys(username)
+        driver.find_element(By.ID, "email").send_keys(email)
         # find password input field and insert password as well
-        driver.find_element(By.ID, "password").send_keys(password)
+        driver.find_element(By.ID, "amount").send_keys(amount)
+        driver.find_element(By.ID, "term").send_keys(term)
+        driver.find_element(By.ID, "income").send_keys(income)
+        driver.find_element(By.ID, "age").send_keys(age)
 
         # click login button
         submit_button = driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
         submit_button.click()
-
         # check if the log in has failed by locating the log in error message
         try:
             WebDriverWait(driver, 2).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[style="color: red"]')))
