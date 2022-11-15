8f393280cf1c631c06c9611551a9b38df0c8a1e7
ArrickTee <s10198673@connect.np.edu.sg>
2022-11-15 16:14:45 +0800

    added test_loan with screenshots and negative test cases

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ URL = {
     "register": DOMAIN_NAME + "/register-account",
     "login": DOMAIN_NAME,
     "forgot_password": DOMAIN_NAME + "/password-request",
+    "loan": DOMAIN_NAME + "/loans/apply"
 }
 
 allure_results_path = "allure-results"
