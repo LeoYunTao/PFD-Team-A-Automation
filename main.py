e74949592886e3ee70d1f912b1959ef92c579fd3
Leo Yun Tao <Leoyuntao@gmail.com>
2022-11-16 23:11:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from config import allure_results_path, csv_reports_path
 
 def main():
 
+    N_JOBS = 5
+
     os_browser = {
         'windows': ['chrome', 'firefox', 'edge'],
         'linux': ['chrome', 'firefox', 'edge'],
@@ -24,7 +26,7 @@ def main():
 
     os.environ['browsers'] = ','.join(os_browser[current_os])
 
-    REPEAT = 5
+    REPEAT = 8
 
     os.environ['repeat'] = str(REPEAT)
 
@@ -35,7 +37,7 @@ def main():
     # allure-results -> allure-report
 
     retcode = pytest.main(['test_cases/', '--production=true', 
-        f'-nlogical', f'--alluredir={allure_results_path}/{current_os}/',
+        f'-n {N_JOBS}', f'--alluredir={allure_results_path}/{current_os}/',
         f'--csv={csv_reports_path}/test_{current_os}.csv', '--csv-columns=id, function, status, duration, message, parameters_as_columns'])
     #pytest test_cases/ -s -n 5 --csv csv-reports/testswindows.csv --csv-columns id, function, status, duration, message, parameters_as_columns
 
